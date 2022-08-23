ff84e2d7c66a770b7d862feb486f360aff0c58cd
Angus <94235117+angushyx@users.noreply.github.com>
2022-08-24 00:17:16 +0800

    [add] redux config

---
+++ store/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+
+/**
+ * 引入 slice ， 引入名稱統一為 xxxReducer
+ */
+import authReducer from '../slices/auth-slice'
+
+const reducers = combineReducers({
+  /**
+   * Reducer 放這裡
+   */
+  authReducer,
+})
+
+const store = configureStore({
+  reducer: reducers,
+})
+
+export default store
