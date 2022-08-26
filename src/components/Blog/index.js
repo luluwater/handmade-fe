6ba005fdcbf4d5dd262bf4950712e01d7f9cfd0a
Angus <94235117+angushyx@users.noreply.github.com>
2022-08-26 12:42:55 +0800

    feature: use swiper create slider

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import images from '../../image'
+// import Carousel from 'react-bootstrap/Carousel'
+// import { motion } from 'framer-motion'
+import './Blog.scss'
 import { Link, Outlet } from 'react-router-dom'
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { useSwiper } from 'swiper/react'
+import 'swiper/scss'
+import 'swiper/scss/navigation'
+import 'swiper/scss/pagination'
+import 'swiper/scss/autoplay'
+
 const BlogLayout = () => {
+  const swiper = useSwiper()
+
+  console.log('hook swiper', swiper)
+
   return (
     <>
+      <Swiper
+        className="mb-12"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: false }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        {images.map((img) => (
+          <SwiperSlide className="banner" key={img}>
+            <img className="banner-img" src={img} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <button className="btn btn-primary" onClick={() => swiper.slideNext()}>
+        Slide to the next slide
+      </button>
+
       <nav className="nav-bar text-center">
         <ul className="text-center list-unstyled">
           <li className="">
@@ -23,6 +62,7 @@ const BlogLayout = () => {
           </li>
         </ul>
       </nav>
+
       <Outlet />
     </>
   )
