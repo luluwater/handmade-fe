45ee514d77c3ba453d4c5acecb7347ab39459087
RJ chen <cloudbay12@gmail.com>
2022-08-30 15:15:45 +0800

    Nav update

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,28 +1,83 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../../../src/assets/home/HANDMADE_LOGO.png'
+import './Navbar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
   return (
     <>
-      <nav className="nav-bar bg-black text-white text-center d-flex justify-content-around">
-        <h1 className="text-center">歡迎來到首首</h1>
-        <ul className="d-flex list-unstyled gap-3 align-items-center justify-content-center gap-2">
-          <li>
-            <Link className="text-white btn btn-warning " to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="text-white btn btn-warning " to="blog">
-              GO TO BLOG
-            </Link>
-          </li>
-          <li>
-            <Link className="text-white btn btn-warning" to="login">
-              Login
-            </Link>
-          </li>
-        </ul>
+      <nav className="navbar mx-5">
+        <img src={Logo} alt="" />
+        <div className="d-flex justify-content-center align-items-center">
+          <form action="" className="flex">
+            <input type="text" className="searchInput" />
+            <button type="submit" className='border-0'>
+              <FontAwesomeIcon
+                icon="fa-solid fa-magnifying-glass"
+                size="xl"
+                className="text-secondary ms-2 me-3"
+                fixedWidth
+              />
+            </button>
+          </form>
+
+          <Link to="/">
+            <FontAwesomeIcon
+              icon="fa-solid fa-cart-shopping"
+              size="xl"
+              className="text-secondary mx-3"
+              fixedWidth
+            />
+          </Link>
+
+          <Link to="/">
+            <FontAwesomeIcon
+              icon="fa-solid fa-user"
+              size="xl"
+              className="text-secondary mx-3"
+              fixedWidth
+            />
+          </Link>
+
+          <ul className="list-unstyled d-flex nav_list">
+            <li>
+              <Link to="/" className="nav_link">
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                STORE
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                COURSE
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                SHOP
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                MAP
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                BLOG
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="nav_link">
+                ABOUT
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   )
