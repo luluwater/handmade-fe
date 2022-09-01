cb334e2441d7f5fe8d9caeb701a37c3de3d199d8
lulu <water0452@gmail.com>
2022-09-01 20:56:59 +0800

    update userpage

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,21 @@
-import AboutBanner from './AboutBanner'
-import AboutDetails from './AboutDetails'
-// import FilterPage from './FilterPage'
-// import UserCard from './UserCard'
-// import UserAccount from './UserAccount'
+// import AboutBanner from './AboutBanner'
+// import AboutDetails from './AboutDetails'
+import FilterPage from './FilterPage'
+import UserCard from './UserCard'
+import UserAccount from './UserAccount'
 import { Row, Col, Container } from 'react-bootstrap'
+import UserPassword from './UserPassword'
 
 const About = () => {
   return (
     <>
-      <AboutBanner />
-      {/* <FilterPage />
+      {/* <AboutBanner /> */}
+      <FilterPage />
       <Container className="d-flex">
         <UserCard />
         <UserAccount />
-      </Container> */}
-      <AboutDetails />
+      </Container>
+      {/* <AboutDetails /> */}
     </>
   )
 }
