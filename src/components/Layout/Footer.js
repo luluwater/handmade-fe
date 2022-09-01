cb339b51c37a5c4d75ddd672a98d01995a99c9e5
RJ Chen <cloudbay@gmail.com>
2022-09-01 14:58:39 +0800

    Nav & footer update RWD

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Container } from 'react-bootstrap'
 import './Footer.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -12,8 +12,8 @@ import {
 const Footer = () => {
   return (
     <>
-      <div className="footer">
-        <ul className="list-unstyled d-flex justify-content-center align-items-center py-10 footer_icon_ul">
+      <Container fluid className="footer">
+        <ul className="list-unstyled d-flex justify-content-center align-items-center footer_icon_ul">
           <li>
             <FontAwesomeIcon
               icon={faFacebookF}
@@ -48,39 +48,59 @@ const Footer = () => {
           </li>
         </ul>
 
-        <Row className="d-flex justify-content-center align-items-center footer_row1">
-          <Col className="footer_cursor p-0">會員制度</Col>
-          <Col className="footer_cursor p-0">服務條款</Col>
-          <Col className="footer_cursor p-0">訂閱電子報</Col>
-          <Col className="footer_cursor p-0" md={1}>品牌招募</Col>
-        </Row>
-
-        <Row className="d-flex justify-content-start align-items-center footer_row1 mt-9">
-          <Col className="footer_shopping_note px-0" md={7}>
-            <p className="">購物須知</p>
-
-            <div className="d-flex mt-3 ">
-              <p className="me-5 mb-2 footer_cursor">退換貨說明</p>
-              <p className="me-5 mb-2 footer_cursor">常見問題</p>
-              <p className="me-5 mb-2 footer_cursor">網站使用條款</p>
-            </div>
-            <div className="d-flex ">
-              <p className="me-5 footer_cursor">隱私權政策</p>
-              <p className="me-5 footer_cursor">物流配送</p>
-              <p className="me-5 footer_cursor">付款說明</p>
-            </div>
+        <Row className="d-flex footer_row1">
+          <Col xs={6} md={3} className="footer_cursor footer_rwd_m">
+            會員制度
           </Col>
-          <Col className="footer_shopping_note" md="auto">
-            <p className="">聯絡我們</p>
-            <div className="mt-3">
-              <p className="me-5 mb-2">service@handmade.com</p>
-            </div>
-            <div>
-              <p className="me-5">( 03 ) - 4567899</p>
-            </div>
+          <Col xs={6} md={3} className="footer_cursor footer_rwd_m">
+            服務條款
+          </Col>
+          <Col xs={6} md={3} className="footer_cursor footer_rwd_m">
+            訂閱電子報
+          </Col>
+          <Col xs={6} md={1} className="footer_cursor footer_rwd_m">
+            品牌招募
+          </Col>
+          <Col
+            xs={6}
+            md={6}
+            className="footer_detail footer_shoppingNote  order-1 order-sm-0"
+          >
+            <p>購物須知</p>
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            className="footer_detail footer_rwd_m order-0 order-sm-0"
+          >
+            聯絡我們
+          </Col>
+          <Col
+            xs={6}
+            md={6}
+            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail order-2 order-sm-0"
+          >
+            <p className="footer_cursor">退換貨說明</p>
+            <p className="footer_cursor">常見問題</p>
+            <p className="footer_cursor">網站使用條款</p>
+          </Col>
+          <Col xs={6} md={4} className="footer_smallWord order-0 order-sm-0">
+            <p>service@handmade.com</p>
+          </Col>
+          <Col
+            xs={6}
+            md={6}
+            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail order-3 order-sm-0"
+          >
+            <p className=" footer_cursor">隱私權政策</p>
+            <p className=" footer_cursor">物流配送</p>
+            <p className=" footer_cursor">付款說明</p>
+          </Col>
+          <Col xs={6} md={4} className="footer_smallWord ">
+            <p className="order-0 order-sm-0 ">( 03 ) - 4567899</p>
           </Col>
         </Row>
-      </div>
+      </Container>
     </>
   )
 }
