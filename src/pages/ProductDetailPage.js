c42821a6ac42a5da479f7e46ee9a5aa968e548cb
zed <z072614@gmail.com>
2022-09-02 18:12:29 +0800

    pics swiper

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import ProductPic from '../components/P&C-Detail/ProductPic'
 
 const ProductDetailPage = () => {
   return (
     <>
-      <h1>ProductDetailPage</h1>
+      <ProductPic />
     </>
   )
 }
