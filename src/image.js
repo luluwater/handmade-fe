22a25205426b360635696ce4bdd1ba346eb2cf95
RJ Chen <cloudbay@gmail.com>
2022-09-07 14:18:44 +0800

    merge 1

--- src/image.js
+++ src/image.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-// import image1 from './assets/blog/blog_banner_1.jpg'
-// import image2 from './assets/blog/blog_banner_2.jpg'
-// import image3 from './assets/blog/blog_banner_3.jpg'
-
-// export default [image1, image2, image3]
-=======
 import image1 from './assets/blog/blog_banner_1.jpg'
 import image2 from './assets/blog/blog_banner_2.jpg'
 import image3 from './assets/blog/blog_banner_3.jpg'
-import productBanner from './assets/store_banner/store_floral_13.jpg'
+// import productBanner from './assets/store_banner/store_floral_13.jpg'
 
 export default [image1, image2, image3]
-export { productBanner }
+// export { productBanner }
 
->>>>>>> f26a1917cc275bdb9d000cdad5378e7fbae1988d
