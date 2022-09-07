fa9384d2554deaf82f9371169468d34a0421109d
RJ Chen <cloudbay@gmail.com>
2022-09-07 13:51:07 +0800

    fixed footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -64,40 +64,40 @@ const Footer = () => {
           <Col
             xs={6}
             md={6}
-            className="footer_detail footer_shoppingNote  order-1 order-sm-0"
+            className="footer_detail footer_shoppingNote  footerOrder_1"
           >
             <p>購物須知</p>
           </Col>
           <Col
             xs={6}
             md={4}
-            className="footer_detail footer_rwd_m order-0 order-sm-0"
+            className="footer_detail footer_rwd_m footerOrder_0"
           >
             聯絡我們
           </Col>
           <Col
             xs={6}
             md={6}
-            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail order-2 order-sm-0"
+            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail  footerOrder_2"
           >
             <p className="footer_cursor">退換貨說明</p>
             <p className="footer_cursor">常見問題</p>
             <p className="footer_cursor">網站使用條款</p>
           </Col>
-          <Col xs={6} md={4} className="footer_smallWord order-0 order-sm-0">
+          <Col xs={6} md={4} className="footer_smallWord  footerOrder_0">
             <p>service@handmade.com</p>
           </Col>
           <Col
             xs={6}
             md={6}
-            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail order-3 order-sm-0"
+            className="footer_cursor d-flex footer_smallWord footer_shoppingNote_detail  footerOrder_3"
           >
             <p className=" footer_cursor">隱私權政策</p>
             <p className=" footer_cursor">物流配送</p>
             <p className=" footer_cursor">付款說明</p>
           </Col>
           <Col xs={6} md={4} className="footer_smallWord ">
-            <p className="order-0 order-sm-0 ">( 03 ) - 4567899</p>
+            <p className=" footerOrder_0">( 03 ) - 4567899</p>
           </Col>
         </Row>
       </Container>
