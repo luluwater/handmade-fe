7878a82f3e0a3094a3dc084ce4300e7b61e6f77d
Angus <sickmi14798@gmail.com>
2022-09-08 14:29:05 +0800

    feature:blog image upload v1

--- src/components/Blog/BlogEdit/Editor/index.js
+++ src/components/Blog/BlogEdit/Editor/index.js
@@ -3,8 +3,8 @@ import Form from 'react-bootstrap/Form'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import BalloonEditor from '@ckeditor/ckeditor5-build-balloon'
 
-// const API_URL = 'http://localhost:8080'
-
+const API_URL = 'http://localhost:8080'
+const UPLOAD_ENDPOINT = 'api/upload_files'
 const Editor = ({
   addTitle,
   handleTitleChange,
@@ -17,51 +17,37 @@ const Editor = ({
    * @param {loader} loader
    * @returns 回傳upload 函式
    */
-  // const uploadAdapter = (loader) => {
-  //   return {
-  //     upload: () => {
-  //       return new Promise((resolve, reject) => {
-  //         const body = new FormData()
-  //         loader.file.then((file) => {
-  //           body.append('files', file)
-  //           fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
-  //             method: 'post',
-  //             body: body,
-  //           })
-  //             .then((res) => res.json())
-  //             .then((res) => {
-  //               resolve({
-  //                 default: `${API_URL}/${res.filename}`,
-  //               })
-  //             })
-  //             .catch((err) => {
-  //               reject(err)
-  //             })
-  //         })
-  //       })
-  //     },
-  //   }
-  // }
+  const uploadAdapter = (loader) => {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData()
+          loader.file.then((file) => {
+            body.append('files', file)
+            fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+              method: 'post',
+              body: body,
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                resolve({
+                  default: `${API_URL}/${res.filename}`,
+                })
+              })
+              .catch((err) => {
+                reject(err)
+              })
+          })
+        })
+      },
+    }
+  }
 
-  // function uploadPlugin(editor) {
-  //   editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-  //     return uploadAdapter(loader)
-  //   }
-  // }
-  /**
-   *  TODO 把 addData 傳出去給後端 server route 後端 server 再把取得的 res.body 存進資料庫
-   */
-  // const submitBlog = () => {
-  //   axios({
-  //     method: 'post',
-  //     url: `${API_URL}/upload_blog`,
-  //     data: {
-  //       content: addContent,
-  //     },
-  //   }).then((res) => {
-  //     console.log(res)
-  //   })
-  // }
+  function uploadPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return uploadAdapter(loader)
+    }
+  }
 
   return (
     <>
@@ -77,7 +63,7 @@ const Editor = ({
       <div className="mb-md-12 mb-7">
         <CKEditor
           editor={BalloonEditor}
-          config={{ placeholder: '輸入內容...' }}
+          config={{ placeholder: '輸入內容...', extraPlugins: [uploadPlugin] }}
           data={addContent ? addContent : editContent}
           onReady={(editor) => {
             console.log('Editor is ready to use!', editor)
