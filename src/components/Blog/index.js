76320d044186db717e87ce97687f0eb03a1949b0
Angus <sickmi14798@gmail.com>
2022-09-14 10:15:28 +0800

    feat:daterange Picker well

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -3,15 +3,26 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import BlogList from './BlogList'
+import FilterKeyword from '../Filter/FilterKeyword'
+import Paginate from '../Filter/Paginate'
+import FilterStore from '../Filter/FilterStore/FilterStore'
+import { useGetStoreQuery } from '../../services/storeApi'
+// import { DateRangePicker } from 'react-date-range'
 
 const Blog = () => {
+  const { data } = useGetStoreQuery()
+  console.log(data)
+
   return (
     <>
       <BlogBanner />
       <Container className="mb-12">
         <Row>
           <Col className="border-right" lg={3}>
-            lg=3
+            <FilterKeyword />
+            <FilterStore />
+            {/* <DateRangePicker /> */}
+            <Paginate />
           </Col>
           <Col lg={9}>
             <BlogList />
