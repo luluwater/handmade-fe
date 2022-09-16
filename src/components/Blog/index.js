2508b099d76d509afdd5d84697172b5d492d48ba
Angus <sickmi14798@gmail.com>
2022-09-16 18:42:49 +0800

    UI:NEW article

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -3,15 +3,16 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import BlogList from './BlogList'
+import { Link } from 'react-router-dom'
 import FilterKeyword from '../Filter/FilterKeyword'
 import Paginate from '../Filter/Paginate'
 import FilterStore from '../Filter/FilterStore/FilterStore'
-import { useGetStoreQuery } from '../../services/storeApi'
-// import { DateRangePicker } from 'react-date-range'
-
+import { useGetBlogQuery } from '../../services/blogApi'
+import { IMG_URL } from '../../utils/config'
 const Blog = () => {
-  const { data } = useGetStoreQuery()
-  console.log(data)
+  const { data } = useGetBlogQuery('all')
+
+  const newData = data?.slice(0, 3)
 
   return (
     <>
@@ -20,8 +21,29 @@ const Blog = () => {
         <Row>
           <Col className="border-right" lg={3}>
             <FilterKeyword />
+
+            <div className="filter">
+              <h5 className="filter_title">最新文章</h5>
+            </div>
+            {newData?.map((item) => {
+              const imgUrl = item.img_url[0].img_name
+              console.log(item)
+              return (
+                <Row className="mb-md-5">
+                  <Col className="d-flex align-items-center">
+                    <Link to={`/blog/${item.blog_id}`}>
+                      <img src={`${IMG_URL}/${imgUrl}`} alt="new blog" />
+                    </Link>
+                  </Col>
+                  <Col className="d-flex align-items-center">
+                    <div className="text-cut ">{item.title}</div>
+                  </Col>
+                </Row>
+              )
+            })}
+
             <FilterStore />
-            {/* <DateRangePicker /> */}
+
             <Paginate />
           </Col>
           <Col lg={9}>
