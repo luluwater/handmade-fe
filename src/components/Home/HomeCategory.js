b72a87bc2f42d3c9c4fa4ee21982fbc48c8aefb0
zed <z072614@gmail.com>
2022-09-20 14:05:26 +0800

    home update

--- src/components/Home/HomeCategory.js
+++ src/components/Home/HomeCategory.js
@@ -24,7 +24,7 @@ function HomeCategory() {
   ]
   return (
     <>
-      <div className="d-flex justify-content-between home_category ">
+      <div className="d-flex justify-content-between home_category mt-10">
         {Category.map((v, i) => {
           return (
             <div key={v.img} className="home_category_box">
