460112fcc358b2021ad674871c650a885654f97f
zed <z072614@gmail.com>
2022-09-26 14:05:19 +0800

    PnC card & comment avatar

--- src/components/ProductDetail/ProductCard.js
+++ src/components/ProductDetail/ProductCard.js
@@ -1,9 +1,9 @@
 import cart from '../../assets/cart.svg'
+import { React, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Col, Container } from 'react-bootstrap'
 import { Navigation } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { React, useEffect, useState } from 'react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/effect-fade'
@@ -17,42 +17,38 @@ import {
 
 import { useDispatch } from 'react-redux'
 import { addProductCart } from '../../slices/productCart-slice'
-export function scrollToTop() {
-  window.scrollTo(0, 0)
+function getImgsRouter(img_name, category_en_name, id) {
+  const baseRouter = `assets/product/product`
+  const router = `${baseRouter}_${category_en_name}_${id}/`
+  const routers = img_name?.map((v) => {
+    return router + v
+  })
+  return routers
 }
 
 function ProductCard() {
   const [card, setCard] = useState([])
   const { data } = useGetProductListQuery()
+  const userId = JSON.parse(localStorage.getItem('user'))?.user.id
+
   useEffect(() => {
     if (data) {
-      if (card.length == 0) {
+      if (card.length === 0) {
         let newData = [...data]?.sort(() => 0.5 - Math.random()).slice(0, 4)
         setCard(newData)
       } else {
         let newData = card.map((c) => {
           return data.find((product) => {
-            return product.id == c.id
+            return product.id === c.id
           })
         })
         setCard([...newData])
       }
     }
   }, [data])
-
-  function getImgsRouter(imgsName, category, productId) {
-    const baseRouter = `assets/product/product`
-    const router = `${baseRouter}_${category}_${productId}/`
-    const routers = imgsName?.map((v) => {
-      return router + v
-    })
-    return routers
-  }
-
   const [addUserFavoriteProduct] = useAddUserFavoriteProductMutation()
   const [removeUserFavoriteProduct] = useRemoveUserFavoriteProductMutation()
   const dispatch = useDispatch()
-  const userId = JSON.parse(localStorage.getItem('user'))?.user.id
 
   return (
     <>
@@ -61,7 +57,8 @@ function ProductCard() {
       </h4>
 
       <Container className="product_detail_card mb-12 w-100 d-flex">
-        {data.map((v, i) => {
+        {card.map((v, i) => {
+          console.log('card', v)
           return (
             <Col
               md={3}
@@ -70,7 +67,7 @@ function ProductCard() {
               key={v.id}
             >
               {/* ========== 商品照片 ========== */}
-              <a href={`/product/detail/${v.id}`} onClick={scrollToTop}>
+              <a href={`/product/detail/${v.id}`}>
                 <Swiper
                   modules={[Navigation]}
                   navigation
@@ -80,29 +77,29 @@ function ProductCard() {
                   loop={true}
                   className="product_detail_card_swiper rounded shadow"
                 >
-                  {getImgsRouter?.map((v2, i2) => {
-                    return (
-                      <SwiperSlide key={i2}>
-                        <img
-                          className="swiper-slide product_detail_card_img"
-                          src={require(`../../../${v2}`)}
-                          alt="products"
-                        />
-                      </SwiperSlide>
-                    )
-                  })}
+                  {getImgsRouter(v.img_name, v.category_en_name, v.id)?.map(
+                    (v2, i2) => {
+                      console.log('getImgsRouter', v2)
+                      return (
+                        <SwiperSlide key={i2}>
+                          <img
+                            className="swiper-slide product_detail_card_img"
+                            src={require(`../../` + v2)}
+                            alt="products"
+                          />
+                        </SwiperSlide>
+                      )
+                    }
+                  )}
                 </Swiper>
               </a>
-
               {/* ========== 商品照片 ========== */}
               <div className="d-flex justify-content-between">
                 <div>
-                  {/* <Link to={v.storeLink}> */}
                   <p className="product_detail_card_store m-2 text-truncate">
-                    <small>| {v.store} |</small>
+                    <small>| {v.store_name} |</small>
                   </p>
-                  {/* </Link> */}
-                  <a href={`/product/detail/${v.id}`} onClick={scrollToTop}>
+                  <a href={`/product/detail/${v.id}`}>
                     <h6 className="product_detail_card_text m-1 fw-bold">
                       {v.name}
                     </h6>
@@ -117,6 +114,7 @@ function ProductCard() {
                 <div className="d-flex align-items-center me-2">
                   <button
                     onClick={() => {
+                      if (!userId) return (window.location.href = '/login')
                       if (v.isFavorite) {
                         removeUserFavoriteProduct({
                           productId: v.id,
@@ -124,8 +122,8 @@ function ProductCard() {
                       } else {
                         addUserFavoriteProduct({
                           productId: v.id,
-                          storeId: v.storeId,
-                          categoryId: v.categoryId,
+                          storeId: v.store_id,
+                          categoryId: v.category_id,
                         })
                       }
                     }}
@@ -142,9 +140,9 @@ function ProductCard() {
                         addProductCart({
                           productId: v.id,
                           name: v.name,
-                          imgs: v.imgs,
+                          imgs: v.img_name,
                           price: Number(v.price),
-                          category: v.category,
+                          category: v.category_en_name,
                           amount: v.amount,
                           quantity: 1,
                         })
