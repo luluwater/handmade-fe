eae1bae8f2a6071e47f377a5deccaad1b195e6be
Angus <sickmi14798@gmail.com>
2022-09-29 13:30:15 +0800

    merge

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -67,51 +67,6 @@ const Blog = () => {
 
   return (
     <>
-<<<<<<< HEAD
-      <Swiper
-        className="mb-12"
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={0}
-        slidesPerView={1}
-        autoplay
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: false }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        {images.map((img) => (
-          <SwiperSlide className="banner" key={img}>
-            <img className="banner-img" src={img} alt="" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <button className="btn btn-primary" onClick={() => swiper.slideNext()}>
-        Slide to the next slide
-      </button>
-
-      <nav className="nav-bar text-center">
-        <ul className="text-center list-unstyled">
-          <li className="">
-            <Link className="text-black btn btn-primary mb-2" to="/">
-              go HOME
-            </Link>
-          </li>
-          <li>
-            <Link className="text-black btn btn-primary  mb-2" to="1234">
-              BLOG DETAIL
-            </Link>
-          </li>
-          <li>
-            <Link className="text-black btn btn-primary  mb-2" to="edit">
-              BLOg editor
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-=======
       <BlogBanner />
       <Container className="mb-12">
         <Row className="justify-content-around">
@@ -147,7 +102,6 @@ const Blog = () => {
           </Col>
         </Row>
       </Container>
->>>>>>> develop
     </>
   )
 }
